5fa3a9c033b229d6909f189eb4ee8d26e0804735
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2015-09-01 15:37:24 +0530

    qcom : corrected the  qti-testscripts.sh to be part of build
    update the  makefile to include the  qti-testscript.sh to be part
    of the build.
    
    Change-Id: I5a3bdd54d4b1222bcb68c4911d1992720b37a98a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -217,6 +217,9 @@ LOCAL_MODULE       := init.qcom.testscripts.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.testscripts.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.debug-8976.sh
 LOCAL_MODULE_TAGS  := debug
 LOCAL_MODULE_CLASS := ETC
