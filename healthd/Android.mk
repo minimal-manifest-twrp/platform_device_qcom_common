a860cc5dccad1b6b10fb7b63f3d040bd2391b37d
Rajshekar Eashwarappa <reashw@codeaurora.org>
2016-03-29 12:51:20 +0530

    common: Enable JACK by default for all modules.
    1. Enable JACK.
    2. Disable DEVICE_OVERLAYS, IMS, VT & Healthd
    3. Compilation issue fix in Power module.
    
    Change-Id: I11ff50b09387f2058f2db90ea714c4042421ef67

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
+ifeq (0,1)  # disabling temporarily
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp healthd_msm_alarm.cpp
 LOCAL_MODULE := libhealthd.msm
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
 include $(BUILD_STATIC_LIBRARY)
+endif
