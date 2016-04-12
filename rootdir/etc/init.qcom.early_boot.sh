d5330b62131ccb3af10dfa3c84889acb50459b4a
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2016-04-12 12:55:34 +0530

    common: choose target specific media xml's
    Define media.msm8956hw and media.settings.xml to choose
    target specific media_codecs.xml and media_profiles.xml
    
    Change-Id: I99448f8c17538fda070766e2a8041e8b37b2bffc

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -172,7 +172,7 @@ case "$target" in
         ;;
       "msm8952")
         case "$soc_hwid" in
-                264|274|277)
+                264|274)
                     setprop ro.sf.lcd_density 480
                     ;;
                 278)
@@ -190,7 +190,7 @@ case "$target" in
                         fi
                     fi
                     ;;
-                266)
+                266|277)
                     setprop ro.sf.lcd_density 480
                     setprop media.msm8956hw 1
                     setprop media.settings.xml /etc/media_profiles_8956.xml
