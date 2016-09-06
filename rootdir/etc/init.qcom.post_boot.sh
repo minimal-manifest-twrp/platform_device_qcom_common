d1bc72f0de7d4d71b720c85fd76e4cf531eb509e
Venumadhav Kurva <kurva@codeaurora.org>
2016-09-06 23:19:23 +0530

    post_boot: Disable L2 GDHS low power mode for 8976
    Change-Id: I47872e382ad62d7e93fa424c730c106c007a2efe

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1246,6 +1246,9 @@ case "$target" in
 
                 # Enable Low power modes
                 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+                # Disable L2 GDHS on 8976
+                echo N > /sys/module/lpm_levels/system/a53/a53-l2-gdhs/idle_enabled
+                echo N > /sys/module/lpm_levels/system/a72/a72-l2-gdhs/idle_enabled
 
                 # Enable sched guided freq control
                 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
