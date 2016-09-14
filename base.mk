f89d91d17e206b88d562535ffe3f38ab4eccbc9f
Vikash Garodia <vgarodia@codeaurora.org>
2016-09-14 15:46:04 +0530

    common: Remove the library from packaging
    The code behind the library being removed,
    is deprecated.
    
    Change-Id: Ic3c8cb6cef3d664fe16d89f0c7bf2d61d0828c7c

--- base.mk
+++ base.mk
@@ -504,7 +504,6 @@ MM_VIDEO += liblasic
 MM_VIDEO += libOmxSwVencMpeg4
 MM_VIDEO += libOmxVdec
 MM_VIDEO += libOmxVdecHevc
-MM_VIDEO += libOmxVdpp
 MM_VIDEO += libOmxVenc
 MM_VIDEO += libOmxVidEnc
 MM_VIDEO += libOmxSwVencHevc
