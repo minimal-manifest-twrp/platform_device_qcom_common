7f3f1b6d69862984ce2d2ef836fee32d57d20829
Ashay Jaiswal <ashayj@codeaurora.org>
2016-09-26 22:38:21 +0530

    healthd: enable compilation of  libhealthd.msm library
    Enable compilation of libhealthd.msm library and update
    the directory path to include headthd specific header files.
    
    Change-Id: Ie46c74c9627a1a86c68f1d94523e79cf71f24ba1

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,9 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-ifeq (0,1)  # disabling temporarily
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp healthd_msm_alarm.cpp
 LOCAL_MODULE := libhealthd.msm
 LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
+LOCAL_C_INCLUDES := system/core/healthd/include/healthd/ bootable/recovery
 include $(BUILD_STATIC_LIBRARY)
-endif
